b8ccb83d0b6b13c8a9326297e3c80720d4444423
unknown <newmohib@gmail.com>
2019-12-13 21:07:03 +0600

    update file

--- textFileRead.sh
+++ textFileRead.sh
@@ -4,4 +4,31 @@ source test.txt
 
 echo "$PASSWORD"
 
+# is test.txt file exist
+echo "PASSWORD="123456"" > test.txt
+
+echo "$PASSWORD" < test.txt
+ 
+# if [ $? -eq 0 ]; then
+#   echo "Hurrey. it works"
+# else
+#   echo "Sorry, can't write test.txt"
+# fi
+
+
+
+# is test.txt  file exist
+
+
+
+
+STRING="tecadmin"
+ 
+if grep ${STRING} test.txt
+then
+  echo "Yeah! string found"
+else
+  echo "Ooooh, no matching string found"
+fi
+
 read -p "Server Run .." yn
